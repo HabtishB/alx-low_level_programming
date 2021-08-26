8eccae67db3009ceb277f6e001d8e6e40036922b
HabtishB <behabtm@gmail.com>
2021-08-26 14:15:30 -0700

    4-puts.c modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
-* main - Entry point
+* main: Entry point
 * Return: Always 0 (Success)
 */
-int main(void)
+int main (void)
 
 {
-char str1[60] = "\"programming is like building a multilingual puzzle";
+char str1[60] = "\"programming is like building a multilingual puzzle \n";
 
 puts(str1);
 return (0);
