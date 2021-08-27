8c1343c874d09f63a80035f3b1baa34094530d75
HabtishB <behabtm@gmail.com>
2021-08-27 15:47:13 -0700

    new if, else if files adde

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-is the main function.
+ *
+ * Return: always returns 0 or 1
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
---
+++ sample.c
@@ -0,0 +1,11 @@
+#include <stdio>
+#include <stdlib>
+/**
+ * main-is the main function
+ *
+ * Return: returns the value
+ */
+int main(void)
+{
+
+}
