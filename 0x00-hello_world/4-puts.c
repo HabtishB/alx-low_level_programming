5b840ac8a6ea373f817de861d4af16749badeb6a
HabtishB <behabtm@gmail.com>
2021-08-26 22:03:24 -0700

    4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-
 #include <stdio.h>
 /**
  * main-function: Entry point
  *
  * Return:always returns 0 always
- */
+*/
 int main(void)
 {
-char str1[60] = "\"programming is like building a multilingual puzzle";
-puts(str1);
-return(0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
