54324ed90c3e63899b336339f11f9416eb03f65e
HabtishB <behabtm@gmail.com>
2021-08-27 05:55:35 -0700

    6-size.c modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,14 @@
 /**
  * main-this is the main function
  *
- * Return: always returns a value of zero
- */ 
+ * Return:always returns a value of zero
+*/
 int main(void)
 {
-
 printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-
 return (0);
 }
