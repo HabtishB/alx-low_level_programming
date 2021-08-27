8a3300990b300a2909cef218dd54448d93d0ebb4
HabtishB <behabtm@gmail.com>
2021-08-27 05:21:45 -0700

    5 printf added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main-function: Entry point
+ *
+ * Return:always returns 0 always
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, \n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
