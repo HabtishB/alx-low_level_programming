b0adc6faf132af189240270ced0f0e77de232ffb
HabtishB <behabtm@gmail.com>
2021-08-30 09:49:32 -0700

    101-quote added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <string.h>
 /**
  * main-function: Entry point
  *
@@ -8,6 +9,6 @@
 */
 int main(void)
 {
-  write(1, "and that piece of art is useful \"- Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful \"- Dora Korpar, 2015-10-19\n"));
+write(1, "and that piece of art is useful \"- Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful \"- Dora Korpar, 2015-10-19\n"));
 return (0);
 }
