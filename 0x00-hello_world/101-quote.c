47b551a2fd10c242aef5b2157613949b42938c3d
HabtishB <behabtm@gmail.com>
2021-08-30 09:40:38 -0700

    101 added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+/**
+ * main-function: Entry point
+ *
+ * Return:always returns 0 always
+*/
+int main(void)
+{
+  write(1, "and that piece of art is useful \"- Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful \"- Dora Korpar, 2015-10-19\n"));
+return (0);
+}
