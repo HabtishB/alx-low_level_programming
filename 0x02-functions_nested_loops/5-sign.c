6bfbca566ba5b07e7823e1df2416eb2b341faba9
HabtishB <behabtm@gmail.com>
2021-09-01 11:21:58 -0700

    Both 5-sign and main.h are updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
-#include "holberton.h"
 #include "main.h"
 /**
- *main-check the code for ALX students
- *
- *Return: always returns zero
+ * print_sign- print the sign of the number and returns 1,
+ * if the number is positive, 0 if zero or -1 if negative.
+ * @c: is the integer value
+ * Return: always returns zero
  */
 
 int print_sign(int c)
 {
-if (c >= 0)
-  {
-    putchar('+');
-    return (1);
+if (c > 0)
+{
+_putchar('+');
+return (1);
 else if (c == 0)
-  {
-    putchar('0');
-    return (0);
-  }
+{
+_putchar('0');
+return (0);
+}
 else
-  {
-    putchar('-');
-    return (-1);
-  }
- }
+{
+_putchar('-');
+return (-1);
+}
+}
