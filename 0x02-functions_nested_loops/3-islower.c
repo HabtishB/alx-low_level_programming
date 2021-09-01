11d2e8e1102e79f4e5b4d8b6653afcbf5a7275e2
HabtishB <behabtm@gmail.com>
2021-09-01 06:38:32 -0700

    3-islower is added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *main-check the code for ALX students
+ *_islower-changes all the alphabets into lower case
  *
  *Return: always returns zero
  */
 
 int _islower(int c)
 {
-
 char a = 'a';
-for ( ; a < 'z'; a++ )
-  if (c == a)
-    return (1);
-  else
-    return (0);
+for (; a < 'z'; a++)
+if (c == a)
+return (1);
+else
+return (0);
 }
