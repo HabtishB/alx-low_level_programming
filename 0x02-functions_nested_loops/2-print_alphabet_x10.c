257318aee7fbea006d28fcc1529aba66cb0b7247
HabtishB <behabtm@gmail.com>
2021-08-31 22:19:14 -0700

    2 print alphabet added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,14 @@
  *
  *Return: always returns zero.
  */
-
 void print_alphabet_x10(void)
 {
-  int i = 0;
-  char a = 'a';
+int i = 0;
+char a = 'a';
 for (; i < 10; i++)
 {
-for (; a <='z'; a++)
-{
-_putchar(a);
-}
-_putchar('\n');
+for (; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
 }
 }
