cb40e2b0db504433c7f9ee04906164206558972a
HabtishB <behabtm@gmail.com>
2021-09-01 12:21:00 -0700

    both 7-print last digit and main.h are updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 #include "main.h"
 /**
- *main-check the code for ALX students
- *
+ * print_last_digit- prints the last digit and also returns the digit
+ *@c: is the value of the integer
  *Return: always returns zero
  */
 
 int print_last_digit(int c)
 {
-  char d = c % 10;
-  if (d > 0)
-    {
-      _putchar('0' + d);
-      return (d);
-    }
-  else
-    {
-      _putchar('0' + (-1 * d));
-      return (-1 * d);
-    }
+char d = c % 10;
+if (d > 0)
+{
+_putchar('0' + d);
+return (d);
+}
+else
+{
+_putchar('0' + (-1 * d));
+return (-1 * d);
+}
 }
