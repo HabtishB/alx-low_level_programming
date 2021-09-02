ec1520b8f9ddfdd4373b9101d72dd7c6e6ccebc4
HabtishB <behabtm@gmail.com>
2021-09-02 09:54:57 -0700

    3 and main added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers - prints the numbers from 0 to 9
+ *
+ *Return: always returns zero.
+ */
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
+
