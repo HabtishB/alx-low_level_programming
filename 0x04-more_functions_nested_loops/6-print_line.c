7b488e9ac094d47de268d993ee552c2fcc5271cf
HabtishB <behabtm@gmail.com>
2021-09-03 05:46:54 -0700

    main and 6 added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line- prints a line.
+ *@n: value of an integer
+ *
+ * Returns: none.
+*/
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
