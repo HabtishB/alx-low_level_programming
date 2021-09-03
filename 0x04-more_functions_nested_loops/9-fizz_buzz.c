273a4b19d08c0646019a4e26a37dfe2d5d7bc65b
HabtishB <behabtm@gmail.com>
2021-09-03 13:05:33 -0700

    fizz buzz added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main- prints fizz buzz.
+ *
+ * Returns: 0.
+*/
+
+int main(void)
+{
+int j = 0;
+while (j <= 100)
+{
+if (j < 3)
+printf("%d", j);
+else if ((j % 3 == 0) && (j % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (j % 3 == 0)
+{
+printf("Fizz");
+}
+else if (j % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{   
+printf("%d", j);
+}
+j++;
+}
+printf("\n");
+return (0);
+}
+
