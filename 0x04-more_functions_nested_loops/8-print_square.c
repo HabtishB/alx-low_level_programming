e345a6ef7f372fc90886eeabd3b904766c76eb84
HabtishB <behabtm@gmail.com>
2021-09-03 11:39:13 -0700

    8 and main updated

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square- prints a sqaure.
+ *@size: value of an integer
+ *
+ * Returns: none.
+*/
+
+void print_square(int size)
+{
+int i;
+int j;
+while (j < size && size > '0')
+{
+for (i = 0; i < size; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+j++;
+}
+_putchar(' ')
+}
