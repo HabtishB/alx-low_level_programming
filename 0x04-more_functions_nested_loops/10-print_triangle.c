7a83d3547a3a9003554ff0db8d51fe3658d25f04
HabtishB <behabtm@gmail.com>
2021-09-03 13:37:29 -0700

    both main and 10 added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_trianble- prints a triangle.
+ *@size: value of an integer
+ *
+ * Returns: none.
+*/
+
+void print_triangle(int size)
+{
+int i;
+int j = 0;
+if (size < 0)
+_putchar('\n');
+else
+{
+while (j < size)
+{
+for (i = (size-1); i >= 0; i--)
+{
+if (i <= j)
+_putchar('#');
+else
+_putchar(' ');
+}
+_putchar('\n');
+j++;
+}
+}
