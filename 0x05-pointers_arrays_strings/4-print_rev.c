780e7667a843563cae3fb8c753c36dcfd01df341
HabtishB <behabtm@gmail.com>
2021-09-06 07:19:06 -0700

    4-print is added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * print_rev- prints a string in a reverse order
+ * @s: is a pointer to a string
+ *
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+
+int len = 0;
+int c = len;
+char *cs = s;
+
+while (*cs)
+{
+len++
+cs++;
+}
+
+for (c = len - 1; c >= 0; c--)
+{
+_putchar(*(s + c));
+}
+_putchar('\n');
+}
