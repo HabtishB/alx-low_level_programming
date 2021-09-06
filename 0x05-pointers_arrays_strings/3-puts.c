47683369442f3128c868436a5b0edc63dade45de
HabtishB <behabtm@gmail.com>
2021-09-06 06:52:13 -0700

    3-puts is added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _puts- prints a string followed by a new line
+ * @s: is a pointer to a string
+ *
+ * Return: this returns a string
+ */
+
+void _puts(char *s)
+{
+
+while (*s)
+{
+_putchar(*s);
+s++;
+}
+return (len++);
+}
