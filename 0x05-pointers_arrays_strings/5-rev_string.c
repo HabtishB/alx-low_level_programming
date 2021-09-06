fd97cb54d23c9e1ed100fbc6e7b381e2c8ef8f53
HabtishB <behabtm@gmail.com>
2021-09-06 08:56:00 -0700

    rev string added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * rev_string- reverses a string
+ * @s: is a pointer to a string
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+
+int i = 0;
+int j = 0;
+int len;
+char rev[1000];
+
+while (*s)
+{
+i++;
+s++;
+}
+
+len = i;
+
+for (i = len - 1; i >= 0; i--)
+{
+rev[j] = s[i];
+j++;
+}
+
+for (i = 0; i <= len - 1; i++)
+{
+s[i] = rev[i];
+}
+}
