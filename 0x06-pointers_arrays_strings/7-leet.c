3d92a315d8ad62660cf84972735ab050c788ad34
HabtishB <behabtm@gmail.com>
2021-09-09 14:03:06 -0700

    7-leet added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet- translates a string into leet
+ * @s: is a pointer to a string
+ *
+ * Return: returns a char
+ */
+
+char *leet(char *s)
+{
+
+int i, j;
+
+char letters[] = "aAeEoOtTlL";
+char leet_it[] = "4433007711";
+
+
+for (k = 0; s[i] != '\0'; i++)
+for (j = 0; j < 10; j++)
+
+if (s[i] == letters[j])
+s[i] = leet_it[j];
+
+return (s);
+}
