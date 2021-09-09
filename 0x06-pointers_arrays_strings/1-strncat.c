feb23e6b9a9e72788075fadf2e57a78919478427
HabtishB <behabtm@gmail.com>
2021-09-09 02:52:46 -0700

    1-strncat added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat- joins two strings
+ * @dest: is the destination string
+ * @src: is the source string
+ * @n: value of an integer
+ * Return: returns the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
