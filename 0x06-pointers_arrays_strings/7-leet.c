423c5a9a5e29741bfb9c66d086d09fc8d70a6239
HabtishB <behabtm@gmail.com>
2021-09-09 14:04:42 -0700

    7-leet added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,7 +16,7 @@ char letters[] = "aAeEoOtTlL";
 char leet_it[] = "4433007711";
 
 
-for (k = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 for (j = 0; j < 10; j++)
 
 if (s[i] == letters[j])
