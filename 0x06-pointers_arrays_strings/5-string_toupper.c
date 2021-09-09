562db71c3dd2ca4bd79b998fde3bef054a6d1b77
HabtishB <behabtm@gmail.com>
2021-09-09 12:56:45 -0700

    5-string added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper- reverses the array
+ * @s: is a pointer to a string
+ *
+ * Return: returns a char
+ */
+
+char *string_toupper(char *s)
+{
+
+int i = 0;
+
+while (s[j] != '\0')
+if (s[j] > 96 && s[j] < 123)
+s[j] -= 32;
+
+return (s);
+
+}
