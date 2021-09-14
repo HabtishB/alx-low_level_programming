6121efa733635e81079ec35262be8ab33650f962
HabtishB <behabtm@gmail.com>
2021-09-14 08:53:54 -0700

    100 and main are updated

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string- sets the value of a char
+ * @s: pointer memory area
+ * @to: pointer for char to fill
+ *
+ * Return: returns nothing
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
