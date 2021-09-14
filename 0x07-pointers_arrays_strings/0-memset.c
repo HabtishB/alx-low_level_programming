488a8a83248d8fde9ed05d1353fd86fb7181948d
HabtishB <behabtm@gmail.com>
2021-09-14 03:25:16 -0700

    0-memset is added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * _memset- fills the memory with a chosen constant
@@ -13,8 +13,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-int i;
-for (i = 0; i < n; i++)
+unsigned int i;
+for (i = 0; i < n && s[i] != '\0'; i++)
 s[i] = b;
 
 return (s);
