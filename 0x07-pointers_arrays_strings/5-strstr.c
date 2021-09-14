f4309158e40d3318a85f2745df964b1b8bde34bc
HabtishB <behabtm@gmail.com>
2021-09-14 05:19:14 -0700

    5 - strstr added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp- compares strings
+ * @s1: first string
+ * @s2: secong string
+ *
+ * Return: returns the difference between the two
+ */
+
+char _strcmp(char *s1, char *s2)
+{
+int i;
+
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+
+}
+return (0);
+}
