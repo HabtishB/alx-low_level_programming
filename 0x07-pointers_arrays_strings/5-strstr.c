b2e60b231902caa41e0045f7c6640a8b1ee6f860
HabtishB <behabtm@gmail.com>
2021-09-14 07:46:19 -0700

    5-strstr fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,30 @@ int _strcmp(char *s1, char *s2)
 int i;
 
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-{
 
 if (s1[i] != s2[i])
 return (s1[i] - s2[i]);
 
-}
 return (0);
+}
+
+/**
+ * _strstr- searches for substring
+ * @haystack: pointer to check a string
+ * @needle: substring
+ *
+ *Return: a pointer to the first byte
+ */
+
+
+ char *_strstr(char *haystack, char *needle)
+{
+
+int j;
+for (j = 0; haystack[j] != '\0'; j++)
+{
+if (_strcmp((haystack + j), needle) == 0)
+return (haystack + j);
+}
+return (NULL);
 }
