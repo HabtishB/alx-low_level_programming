154eaa048fa5f242249d337a119e38feec4fd1d6
HabtishB <behabtm@gmail.com>
2021-09-14 04:45:04 -0700

    3 strspn added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * _strspn- finds the length of a prefix substring
+ * @s: pointer to a string
+ * @c: byte to check
+ *
+ * Return: returns the number of bytes from accept
+ */
+
+char *_strspn(char *s, char *accept)
+{
+
+unsigned int i, j, gotit, checking;
+
+gotit = 0;
+
+while (s[i] >= '\0')
+{
+checking = 0;
+
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+gotit++;
+checking = 1;
+}
+}
+if (checker == 0)
+return (gotit);
+i++;
+}
+
+return (gotit);
+}
