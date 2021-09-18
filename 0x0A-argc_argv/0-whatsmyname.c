356045d57e0b9233e2c2c803e0bde7e2790e0201
HabtishB <behabtm@gmail.com>
2021-09-18 05:01:10 -0700

    0-fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * main- prints out the name of the program
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguements
+ *
+ * Return: always 0 for success
+ */
+
+
 int main(int argc, char *argv[])
 {
-int i;
-for(i = 0; i < 1; i++)
-  _putchar(atoi(argv[i]));
-
- return (0);
+printf("%s\n", argv[0]);
+(void) argc
+return (0);
 }
