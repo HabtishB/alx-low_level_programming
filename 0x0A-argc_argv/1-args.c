71822d01eb0b7853509044f6fb21fbd0566bc65e
HabtishB <behabtm@gmail.com>
2021-09-18 05:24:20 -0700

    1-args.c was added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main- prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of pointers to string arguements
+ *
+ * Return: always 0 for success
+ */
+
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+(void) argc;
+return (0);
+}
