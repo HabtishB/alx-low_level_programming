b7ae926e454de4883334fe1633178e07d0749296
HabtishB <behabtm@gmail.com>
2021-09-22 05:01:14 -0700

    1-strdup added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup- creates a string duplicate
+ * @str: a string pointer
+ *
+ * Return: returns a pointer to the string chars
+ */
+
+char *_strdup(char *str)
+{
+char *array;
+int i, size = 0;
+
+while (*str)
+{
+size++;
+str++;
+}
+
+array = (char *)malloc((size + 1) * sizeof(char));
+
+if (size == 0 || str == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+array[i] = str[i];
+
+array[size] = '\0';
+return (array);
+}
