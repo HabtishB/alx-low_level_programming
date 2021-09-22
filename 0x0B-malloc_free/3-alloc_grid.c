b6530330f10d8a16c8fe10fd154f83b40ed7c8c9
HabtishB <behabtm@gmail.com>
2021-09-22 12:15:07 -0700

    3-alloc added

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * alloc_grid- creates a 2 dimensional array
+ * @width: width of the two dimensional array
+ * @height: height of the two dimensional array
+ *
+ * Return: returns a pointer to 2-dim array
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **matrix;
+unsigned int i, j;
+
+if (width < 1 || height < 1)
+return (NULL);
+
+matrix = (int **)malloc(height * sizeof(int));
+
+if (matrix == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+matrix[i] = malloc(sizeof(int) * width);
+
+if (matrix[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(matrix[j]);
+
+free(matrix);
+return (NULL);
+}
+
+for (j = 0; j < width; j++)
+matrix[i][j] = 0;
+}
+return (matrix);
+}
+
