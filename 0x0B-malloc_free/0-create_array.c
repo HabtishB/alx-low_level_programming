d4f4b6c0e4c251db59332baaa08c3763cfde29b2
HabtishB <behabtm@gmail.com>
2021-09-22 04:37:44 -0700

    0-create array fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 char *array;
 unsigned int i;
 
-array =(char*)malloc(size * sizeof(char));
+array = (char *)malloc(size * sizeof(char));
 
-if (size == 0)
+if (size == 0 || array == NULL)
 return (NULL);
 
 for (i = 0; i < size; i++)
