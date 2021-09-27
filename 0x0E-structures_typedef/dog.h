ac4739b3d84a76c45d74eb8371b5eae7ba706692
HabtishB <behabtm@gmail.com>
2021-09-27 12:52:38 -0700

    struct dog description was added and fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _dog_
 #define _dog_
 
+/**
+ * struct dog- a structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: returns none
+ */
+
 struct dog
 {
 char *name;
