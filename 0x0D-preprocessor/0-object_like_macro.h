681a7504924e4b0a989fe6f0126032dc0f560daf
HabtishB <behabtm@gmail.com>
2021-09-27 11:06:55 -0700

    preprocessor was added

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef SIZEH
+
+#define SIZE 1024
+
+
+#endif 
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - check the code for ALX school students
+ *
+ * Return: returns always zero
+ *
+ */
+
+int main(void)
+{
+
+printf("%s\n", _FILE_)
+return (0);
+}
