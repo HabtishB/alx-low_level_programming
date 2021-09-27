b89437f80db8f666a37123c2a27f0049ef753e4c
HabtishB <behabtm@gmail.com>
2021-09-27 16:09:26 -0700

    dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,3 +18,20 @@ char *owner;
 };
 
 #endif
+
+
+/**
+ * typedef dot_t- new nick name
+ *
+ *Return: none
+ *
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *name);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d)
+
+#endif
