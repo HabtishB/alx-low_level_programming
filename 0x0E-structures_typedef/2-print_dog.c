6de87f8117f4eefc8e2cfc5e21fcee24f1965adb
HabtishB <behabtm@gmail.com>
2021-09-28 18:47:49 -0700

    2-print_dog.c added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-print ("Name: %s\n", d->name ? d->name : "(nil)");
-print("Age: %6f\n", d->age);
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %6f\n", d->age);
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
