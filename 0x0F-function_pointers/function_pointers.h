d5d6979d5dc20ccae902c2bf31e6b1a1efdd8300
HabtishB <behabtm@gmail.com>
2021-09-29 19:32:36 -0700

    func pointer added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNC_POINTERS_
+#define FUNC_POINTERS_
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void(*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* FUNC_POINTERS */
