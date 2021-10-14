94ead16d471b322ad391650a298945af2aa1a41e
HabtishB <behabtm@gmail.com>
2021-10-13 19:19:18 -0700

    10 delete node fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,35 +10,39 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+listint_t *previous;  
 listint_t *current;
-listint_t *next;
+listint_t *delete;
 unsigned int i = 0;
-
-if (!head || !(*head))
-return (1);
 
 current = *head;
 
-if (!index)
+if (index == 0 && current != NULL)
 {
+previous = *head;
 *head = current->next;
-free(current);
-return (0);
+free(previous);
+return (1);
 }
 
-while (current && i < index - 1)
+if (current == NULL)
+return (-1);
+ 
+while (current != NULL)
 {
+  if (i == (index - 1))
+    {
+      previous = current;
+      current = current->next;
+      delete = current;
+      current = current->next;
+      previous->next = current;
+      free(delete);
+      return (1);
+    }
 current = current->next;
 i++;
 }
 
-if (!current || !(current->next))
 return (-1);
-
-next = current->next->next;
-free(current->next);
-current->next = next;
-
-return (0);
 }
-
