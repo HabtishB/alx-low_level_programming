138a3e8dc3ac2e3ae29540188a0d78089125a71b
HabtishB <behabtm@gmail.com>
2022-02-06 14:57:02 -0800

    2 key index added

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "include.h"
+
+/**
+ * key_index - gives the index of a key
+ *
+ * @key: the key for the index
+ * @size: size of the array of hash table
+ * Return: the index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int hash, index = 0;
+
+hash = hash_djb2(key);
+
+if (size != 0)
+index = hash % size;
+return (index);
+}
