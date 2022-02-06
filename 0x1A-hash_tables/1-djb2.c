b71d9eb9f839c0a727c69e5254b353bf1cf997cd
HabtishB <behabtm@gmail.com>
2022-02-06 14:26:11 -0800

    djb2 added

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include <hash.tables.h>
+
+/**
+ * hash_djb2 - creates a hash with the djb2 algorithm
+ * @str - key to create a hash
+ * Return - the generated hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381
+int c;
+
+while (*str)
+{
+c = *str++;
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+}
+
+return (hash);
+}
